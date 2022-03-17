622f997f6aebe906b98f1659228492c6904c8c4b
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-16 22:15:40 -0300

    Finalizando a tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import './style.css'
 import logo from '../../assets/image/logo.png'
+import { Link } from "react-router-dom";
 
-export default function Login(){
-    return(
+export default function Login() {
+    return (
         <div className='login'>
             <div className='logo-wrapper'>
                 <img src={logo} className='logo'></img>
             </div>
-            <div>
-                <div>
-                    <p>Entre.</p>
-                    <input placeholder="Email"/> 
-                    <input type='password' placeholder="Senha"/> 
-                    <button>Entre</button>
-                    <p>Não tem uma conta? Cadastre-se</p>
+            <div className="input-wrapper">
+                <div className="action-wrapper">
+                    <p className="title-login">Entre.</p>
+                    <input placeholder="Email" className='input-action' />
+                    <input type='password' placeholder="Senha" className='input-action' />
+                    <button className='button-action'>Entre</button>
+                    <p className="sign-action">Não tem uma conta? <Link to='/' className="link-action">Cadastre-se</Link></p>
                 </div>
             </div>
         </div>
