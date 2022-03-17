4434df6c7c8a2ad84032547a6af273b7e7ac6bfa
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-16 21:32:44 -0300

    Desenvolvendo a página de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import './style.css'
+import logo from '../../assets/image/logo.png'
 
 export default function Login(){
     return(
-        <h1>Login</h1>
+        <div className='login'>
+            <div className='logo-wrapper'>
+                <img src={logo} className='logo'></img>
+            </div>
+            <div>
+                <div>
+                    <p>Entre.</p>
+                    <input placeholder="Email"/> 
+                    <input type='password' placeholder="Senha"/> 
+                    <button>Entre</button>
+                    <p>Não tem uma conta? Cadastre-se</p>
+                </div>
+            </div>
+        </div>
     )
 }
