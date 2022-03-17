617905b16641393abc760757c48949373be32c3a
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-16 22:32:57 -0300

    Finalizando o login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -4,6 +4,10 @@ import logo from '../../assets/image/logo.png'
 import { Link } from "react-router-dom";
 
 export default function Login() {
+    function handleClick(){
+        console.log('Logou!')
+    }
+
     return (
         <div className='login'>
             <div className='logo-wrapper'>
@@ -12,10 +16,14 @@ export default function Login() {
             <div className="input-wrapper">
                 <div className="action-wrapper">
                     <p className="title-login">Entre.</p>
-                    <input placeholder="Email" className='input-action' />
-                    <input type='password' placeholder="Senha" className='input-action' />
-                    <button className='button-action'>Entre</button>
-                    <p className="sign-action">Não tem uma conta? <Link to='/' className="link-action">Cadastre-se</Link></p>
+                    <div className="inputs">
+                        <input placeholder="Email" className='input-action first-input'/>
+                        <input type='password' placeholder="Senha" className='input-action' />
+                    </div>
+                    <div className button-link>
+                        <button className='button-action' onClick={() => handleClick()}>Entre</button>
+                        <p className="sign-action">Não tem uma conta? <Link to='/' className="link-action">Cadastre-se</Link></p>
+                    </div> 
                 </div>
             </div>
         </div>
