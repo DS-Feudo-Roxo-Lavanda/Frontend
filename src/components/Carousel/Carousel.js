da1419f93d690c1b1268dc0be4ced2cc1e175a32
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-24 22:02:26 -0300

    Adicionando sombra no título

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -5,16 +5,16 @@ import "./Carousel.css"
 export default function Carousel(){
 
    /* 
-   Função pra buscar os dados no arquivo json
+   Função pra buscar os dados no json
    */
 
     return(
         <>
-            <div className='caurosel'>
+            <div className='carousel'>
                 <div className="show" >
-                    <div className='img-title'>
-                        <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg" className="image" ></img>    
-                        <p className="title">The Batman</p>       
+                    <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg" className="image" ></img>    
+                    <div className='tittle-wrapper'>
+                        <span className="tittle">The Batman</span>  
                     </div>
                 </div>
             </div>
