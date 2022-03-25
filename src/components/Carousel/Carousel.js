06c15056e8658fccf56aa95ec0a27c1464449b05
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-25 19:25:08 -0300

    Adicionando os gêneros na Home

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,16 +9,14 @@ export default function Carousel(){
    */
 
     return(
-        <>
-            <div className='carousel'>
-                <div className="show" >
-                    <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg" className="image" ></img>    
-                    <div className='tittle-wrapper'>
-                        <span className="tittle">The Batman</span>  
-                    </div>
+        <div className='carousel'>
+            <div className="show" >
+                <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg" className="image" ></img>    
+                <div className='tittle-wrapper' >
+                    <span className="tittle">The Batman</span>  
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,13 +16,18 @@ export default function Home() {
                 <input className="search" placeholder="Pesquisar"/>
             </div>
             <div className="home-wrapper">  
-                 <Carousel/>
-                 <Carousel/>
-                 <Carousel/>
-                 <Carousel/>
-                 <Carousel/>
-                 <Carousel/>
-                 <Carousel/>
+                <h2 className="gen-tittle">Lançamentos</h2>
+                    <Carousel className="home_carousel"/>
+                <h2 className="gen-tittle">Animações</h2>
+                    <Carousel className="home_carousel"/>
+                <h2 className="gen-tittle">Ação e Aventura</h2>
+                    <Carousel className="home_carousel"/>
+                <h2 className="gen-tittle">Comédias</h2>
+                    <Carousel className="home_carousel"/>
+                <h2 className="gen-tittle">Drama</h2>
+                    <Carousel className="home_carousel"/>
+                <h2 className="gen-tittle">Terror</h2>
+                    <Carousel className="home_carousel"/>
             </div>
         </div>
     )
