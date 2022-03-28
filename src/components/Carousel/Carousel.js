4f5bf55a7d482b0be26765ef48032286b8cba3e7
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-27 21:55:26 -0300

    Adicionando a api fake para testar o Carrosel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,22 +1,30 @@
 import react from "react";
-import { useEffect } from "react"; 
+import React, { useEffect, useState } from 'react'
 import "./Carousel.css"
 
 export default function Carousel(){
+   
+   /* const[data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://fakerestapi.azurewebsites.net/api/v1/CoverPhotos')
+            .then((Response) => Response.json())
+            .then(setData);
+    }, []);
 
-   /* 
-   Função pra buscar os dados no json
-   */
+    if (!data || !data.lenth) return null*/
 
     return(
-        <div className='carousel'>
-            <div className="show" >
-                <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg" className="image" ></img>    
-                <div className='tittle-wrapper' >
-                    {/*<span className="tittle">The Batman</span>*/}
+        <div className='container'>
+            <div className='carousel'>
+                <div className="show" >
+                    <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg" className="image" ></img>    
+                    <div className='tittle-wrapper' >
+                        {/*<span className="tittle">The Batman</span>*/}
+                    </div>
                 </div>
             </div>
         </div>
+        
     )
 }
 
