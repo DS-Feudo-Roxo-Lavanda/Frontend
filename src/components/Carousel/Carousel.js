615fcf07cdff27c89c2186e61e810cff0d80f45b
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-28 13:57:13 -0300

    Desenvolvendo o Carrosel

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,27 +4,33 @@ import "./Carousel.css"
 
 export default function Carousel(){
    
-   /* const[data, setData] = useState([]);
+    const[data, setData] = useState([]);
     useEffect(() => {
         fetch('https://fakerestapi.azurewebsites.net/api/v1/CoverPhotos')
             .then((Response) => Response.json())
             .then(setData);
     }, []);
 
-    if (!data || !data.lenth) return null*/
+    
+    
 
     return(
         <div className='container'>
-            <div className='carousel'>
-                <div className="show" >
-                    <img src="https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg" className="image" ></img>    
-                    <div className='tittle-wrapper' >
-                        {/*<span className="tittle">The Batman</span>*/}
+             <div className='carousel'>
+            {data.map((show) => {
+                const {id, url} = show;
+                return(
+                    <div className="show" key={id} >
+                        <img src='https://www.themoviedb.org/t/p/w220_and_h330_face/cKNxg77ll8caX3LulREep4C24Vx.jpg' className="image" ></img>    
+                        <div className='tittle-wrapper' >
+                            {/*<span className="tittle">The Batman</span>*/}
+                        </div>
                     </div>
-                </div>
+                );
+            })}
             </div>
         </div>
-        
+    
     )
 }
 
