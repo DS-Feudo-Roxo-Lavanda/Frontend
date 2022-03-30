05e8f196a2649cf3657209424c334258dcb7d9fd
Eric Rabelo <eric.rabelo21@gmail.com>
2022-03-30 15:11:47 -0300

    Adicionando URL em todas as recomendações

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -18,27 +18,27 @@ export default function Home() {
             <div className="home-wrapper">  
                 <h2 className="gen-tittle">Lançamentos</h2>
                     <div className="carousel-wrapper">
-                        <Carousel className="home_carousel"/>
+                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/11/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
                     </div>
                 <h2 className="gen-tittle">Animações</h2>
                     <div className="carousel-wrapper">
-                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/16/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
+                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/12/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
                     </div>
                 <h2 className="gen-tittle">Ação e Aventura</h2>
                     <div className="carousel-wrapper">
-                        <Carousel className="home_carousel"/>
+                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/18/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
                     </div>
-                <h2 className="gen-tittle">Comédias</h2>
+                <h2 className="gen-tittle">Comédia</h2>
                     <div className="carousel-wrapper">
-                        <Carousel className="home_carousel"/>
+                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/14/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
                     </div>
                 <h2 className="gen-tittle">Drama</h2>
                     <div className="carousel-wrapper">
-                        <Carousel className="home_carousel"/>
+                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/20/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
                     </div>
                 <h2 className="gen-tittle">Terror</h2>
                     <div className="carousel-wrapper">
-                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/28/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
+                        <Carousel className="home_carousel" url='https://api.themoviedb.org/3/movie/27/recommendations?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&page=1'/>
                     </div>
                     
             </div>
