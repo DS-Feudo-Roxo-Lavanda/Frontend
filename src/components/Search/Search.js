71f736668094cb1ba68886ec74bbba9ff112da57
Eric Rabelo <eric.rabelo21@gmail.com>
2022-04-03 17:20:11 -0300

    Alterando função fetch na search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,38 +1,32 @@
 import react from "react";
-import  { useState } from 'react'
-import styles from './Search.css'
+import  {useState} from 'react';
+import styles from './Search.css';
+import SearchResults from "./SearchResults";
 
 /*https://api.themoviedb.org/3/search/multi?api_key=5914e1c087d0360b477c8b0ceee
 77bff&language=pt-BR&query=<<aqui vai o que ta no search>>&page=1&include_adult=false*/
 
 export default function Search(){
 
-    /*const [data, setData] = useState([]);*/
+    const [results, setResults] = useState([]);
 
     const handleInputChange = (e) => {
         e.preventDefault();
-        const {value} = e.target;
+        const { value } = e.target;
         if (!value) return;
 
-        const url = 'https://api.themoviedb.org/3/search/multi?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&query='+value+'&page=1&include_adult=false'
+        const url = `https://api.themoviedb.org/3/search/multi?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&query=${value}&page=1&include_adult=false`
         
-        fetch(url).then((response) => response.json())
-        .then(console.log) 
+        fetch(url)
+        .then((response) => response.json())
+        .then(({results}) => setResults(results))
     }
-
+    console.log(results)
     return(
         <div className='container'>
             <div className='search-wrapper'>
-                <input className="search" placeholder="Pesquisar" onChange={handleInputChange}/>
-                <div className='search-results'>
-                    <ul>
-                        <li>Filme 1</li>
-                        <li>Filme 2</li>
-                        <li>Filme 3</li>
-                        <li>Filme 4</li>
-                        <li>Filme 5</li>
-                    </ul>
-                </div>
+                <input className="search" placeholder="Pesquisar" onChange={handleInputChange}/> 
+                <SearchResults/>
             </div>
         </div>
     )
---
+++ src/components/Search/SearchResults.js
@@ -0,0 +1,15 @@
+const SearchResults = () => {
+    return(
+        <div className='search-results'>
+                    <ul>
+                        <li>Filme 1</li>
+                        <li>Filme 2</li>
+                        <li>Filme 3</li>
+                        <li>Filme 4</li>
+                        <li>Filme 5</li>
+                    </ul>
+                </div>
+    )
+}
+
+export default SearchResults
