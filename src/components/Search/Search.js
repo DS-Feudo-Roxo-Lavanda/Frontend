0e97831e1ea0e5fd9eb184b506791b75969c6ea5
Eric Rabelo <eric.rabelo21@gmail.com>
2022-04-03 18:50:35 -0300

    Adicionando algumas funcionalidades na search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -9,13 +9,17 @@ import SearchResults from "./SearchResults";
 export default function Search(){
 
     const [results, setResults] = useState([])
+
     const handleInputChange = (e) => {
         e.preventDefault();
         const { value } = e.target;
-        if (!value) return;
 
+        if (!value) {
+            setResults([])
+            return;
+        }
+    
         const url = `https://api.themoviedb.org/3/search/multi?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&query=${value}&page=1&include_adult=false`
-        
         fetch(url)
         .then((response) => response.json())
         .then(({results}) => setResults(results))
