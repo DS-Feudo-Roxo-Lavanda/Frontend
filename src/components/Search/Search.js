8777b86ce1edabaab1d8619da5848d12c23b2c5b
Eric Rabelo <eric.rabelo21@gmail.com>
2022-04-03 02:37:51 -0300

    Criando função para buscar filmes na API e converter pra json

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,26 @@
 import react from "react"
 import styles from './Search.css'
 
+/*https://api.themoviedb.org/3/search/multi?api_key=5914e1c087d0360b477c8b0ceee
+77bff&language=pt-BR&query=<<aqui vai o que ta no search>>&page=1&include_adult=false*/
+
 export default function Search(){
+
+    const handleInputChange = (e) => {
+        e.preventDefault();
+        const {value} = e.target;
+        if (!value) return;
+
+        const url = 'https://api.themoviedb.org/3/search/multi?api_key=5914e1c087d0360b477c8b0ceee77bff&language=pt-BR&query='+value+'&page=1&include_adult=false'
+        
+        fetch(url).then((response) => response.json())
+        .then(console.log)
+    }
+
     return(
         <div className='container'>
             <div className='search-wrapper'>
-                <input className="search" placeholder="Pesquisar"/>
+                <input className="search" placeholder="Pesquisar" onChange={handleInputChange}/>
                 <div className='search-results'>
                     <ul>
                         <li>Filme 1</li>
