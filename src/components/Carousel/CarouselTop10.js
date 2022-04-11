07c0450bf7f5f84acd9ba35d777d393934df8496
Eric Rabelo <eric.rabelo21@gmail.com>
2022-04-11 18:55:51 -0300

    Adicionando o navigate CarouselTop10

--- src/components/Carousel/CarouselTop10.js
+++ src/components/Carousel/CarouselTop10.js
@@ -1,10 +1,12 @@
 import react from "react";
 import React, { useEffect, useState, useRef } from 'react'
+import { useNavigate } from 'react-router-dom';
 import "./Carousel.css"
 import sliderArrow from "../../assets/image/sliderArrow.png"
 
 export default function Carousel(props){
    
+    const navigate = useNavigate();
     const[data, setData] = useState([]);
     const carousel = useRef(null);
     useEffect(() => {
@@ -32,9 +34,9 @@ export default function Carousel(props){
             <button className="slider-arrow first-arrow" onClick={handleLeftClick}><img src={sliderArrow} alt='Scroll Left'></img></button>
             <div className='top10-carousel' ref={carousel}>
             {data.map((show) => {
-                const {id, title, poster_path } = show;
+                const {id, title, poster_path, overview, release_date, vote_average } = show;
                 return(
-                    <div className="top10-show" key={id} >
+                    <div className="top10-show" key={id} onClick={() => navigate('/Details', {state: {id: id, title: title, poster: poster_path, overview: overview, date: release_date, vote: vote_average}})}>
                         <img ></img>
                         <img src={'https://image.tmdb.org/t/p/w220_and_h330_face' + poster_path} className="image" ></img>    
                         <div className='tittle-wrapper' >
