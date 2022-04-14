1090726325e40e0e9248ca038723e27e3d541afd
Juan-Ricarte <96394878+Juan-Ricarte@users.noreply.github.com>
2022-04-13 22:50:26 -0300

    integracao_front_back

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/image/logo.png"
+import instance from "../../services/instance";
+
 
 export default function Cadastro() {
-    function handleClick() {
-        console.log("Criou!")
-    }
+
+    const[username,setUsername]= useState([])
+    const[email,setEmail]= useState([])
+    const[password,setPassword]= useState([])
+
+    useEffect(()=>{
+        instance.post('/cadastro',{
+            email: email,
+            username: username,
+            password: password
+        })
+        .then(function(response){
+            console.log(response);
+        })
+        .catch(function(error){
+            console.error(error);
+        })
+    },[])
+    
+
+
 
     return(
         <div className="login-cadastro">
@@ -16,12 +36,12 @@ export default function Cadastro() {
                 <div className="action-wrapper">
                     <p className="title-login">Crie sua conta.</p>
                     <div className="inputs">
-                        <input placeholder="Usuário" className="input-action first-input" />
-                        <input placeholder="Email" className="input-action first-input" />
-                        <input placeholder="Senha" type="password" className="input-action" />
+                        <input placeholder="Usuário" className="input-action first-input" username={username} />
+                        <input placeholder="Email" className="input-action first-input" email={email} />
+                        <input placeholder="Senha" type="password" className="input-action" password={password} />
                     </div>
                     <div className="button-link">
-                        <button className="button-action" onClick={() => handleClick()}>Criar</button>
+                        <button className="button-action" >Criar</button>
                         <p className="login-action">Já possui uma conta? <Link to="/" className="link-action">Entrar</Link></p>
                     </div>
                 </div>
