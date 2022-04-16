96b4c6e5216f1047ff68e4b0cfb93a116625bc3e
Eric Rabelo <eric.rabelo21@gmail.com>
2022-04-16 12:51:45 -0300

    Alterando o cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -6,11 +6,12 @@ import instance from "../../services/instance";
 
 export default function Cadastro() {
 
-    const[username,setUsername]= useState([])
-    const[email,setEmail]= useState([])
-    const[password,setPassword]= useState([])
+    const[username, setUsername]= useState([])
+    const[email, setEmail]= useState([])
+    const[password, setPassword]= useState([])
 
-    useEffect(()=>{
+
+    {/*useEffect(()=>{
         instance.post('/cadastro',{
             email: email,
             username: username,
@@ -22,11 +23,31 @@ export default function Cadastro() {
         .catch(function(error){
             console.error(error);
         })
-    },[])
+    },[])*/}
+
+    async function handleSubmit(){
+        const data = {
+            username: username,
+            email: email,
+            password: password}
+            
+            if (username != '' && email != '' && password != ''){
+                const response = await instance.post('/cadastro', data);
+
+                if(response.status === 200){
+                    window.location.href='/home'
+                }else{
+                    alert('Erro ao cadastrar')
+                }
+            }else{
+                alert('Por favor, preencha todos os campos!')
+            }
+           
+
+            console.log(data)
+    }
+
     
-
-
-
     return(
         <div className="login-cadastro">
             <div className="logo-wrapper">
@@ -36,12 +57,30 @@ export default function Cadastro() {
                 <div className="action-wrapper">
                     <p className="title-login">Crie sua conta.</p>
                     <div className="inputs">
-                        <input placeholder="Usuário" className="input-action first-input" username={username} />
-                        <input placeholder="Email" className="input-action first-input" email={email} />
-                        <input placeholder="Senha" type="password" className="input-action" password={password} />
+                        <input 
+                        placeholder="Usuário"
+                        className="input-action first-input"
+                        username={username}
+                        value={username}
+                        onChange={e => setUsername(e.target.value)} />
+                        <input 
+                        placeholder="Email" 
+                        className="input-action first-input" 
+                        email={email} 
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        />
+                        <input 
+                        placeholder="Senha" 
+                        type="password" 
+                        className="input-action" 
+                        password={password}
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        />
                     </div>
                     <div className="button-link">
-                        <button className="button-action" >Criar</button>
+                        <button className="button-action" onClick={handleSubmit}>Criar</button>
                         <p className="login-action">Já possui uma conta? <Link to="/" className="link-action">Entrar</Link></p>
                     </div>
                 </div>
